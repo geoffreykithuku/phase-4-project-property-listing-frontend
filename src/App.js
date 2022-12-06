57b1af26965edad732a88df29ed33947bc3c1ffb
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-12-06 13:41:05 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React from "react";
- import Forms from "./components/forms";
-import Footer from "./components/Footer";
+//  import Forms from "./components/forms";
+// import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      {/* <Forms /> */}
       <Navbar/>
-      <Forms />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <Link to="/" >Home</Link>
-      <Link to="/login" >Login</Link>
-      <Link to="/signup" >Signup</Link>
+      <Link >Home</Link>
+      <Link >Login</Link>
+      <Link >Signup</Link>
     </div>
   );
 };
