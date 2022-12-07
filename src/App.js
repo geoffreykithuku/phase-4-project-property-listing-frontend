f898eb03269f85d0462d2a97bf63ea7248f1308d
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2022-12-07 15:46:49 +0300

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,4 @@
 import "./App.css";
-<<<<<<< HEAD
-import React,{Fragment} from 'react';
-import Home from "./components/Home";
-import Property from "./components/property";
- 
-function App() {
-  return (
-    <div className="App">
-     <Fragment>
-
-
-<Home/>
-<Property/>
-
-</Fragment> 
-=======
 import Forms from "./components/forms";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
@@ -29,7 +13,6 @@ function App() {
         <Route path="/new" element={<NewProperty />} />
         <Route path="/forms" element={<Forms/>} />
       </Routes>
->>>>>>> 357aa46f9a6ea2c1582c54fe0ebefd9729fc6170
     </div>
   );
 }
