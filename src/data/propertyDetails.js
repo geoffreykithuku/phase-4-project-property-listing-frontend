2591998f8a134cdbf42fca54472d8b87945e1db5
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2022-12-07 19:37:23 +0300

    feat: display data from the backend

--- src/data/propertyDetails.js
+++ src/data/propertyDetails.js
@@ -1,63 +1,63 @@
 const PropertyDetail = [
-    {
-        id:"1",
-        Image:"./PropertyImages/house-g36151780b_1280.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    },
-    {
-        id:"2",
-        Image:"./PropertyImages/r-architecture-2gDwlIim3Uw-unsplash.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    },
-    {
-        id:"3",
-        Image:"./PropertyImages/stephan-bechert-yFV39g6AZ5o-unsplash.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    },
-    {
-        id:"4",
-        Image:"./PropertyImages/todd-kent-178j8tJrNlc-unsplash.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    },
-    {
-        id:"5",
-        Image:"./PropertyImages/vu-anh-TiVPTYCG_3E-unsplash.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    },
-    {
-        id:"6",
-        Image:"./PropertyImages/residence-ge8328143b_1920.jpg",
-        description:"lorem",
-        rent:"From $20K",
-        size:"1200 Sqft",
-        bed:"2 Bed",
-        bath:"2 Bathroom",
-       location:"lorem"
-    }
-]
-export default PropertyDetail
+  {
+    id: "1",
+    Image: "./PropertyImages/house-g36151780b_1280.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Kiambu",
+  },
+  {
+    id: "2",
+    Image: "./PropertyImages/r-architecture-2gDwlIim3Uw-unsplash.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Juja",
+  },
+  {
+    id: "3",
+    Image: "./PropertyImages/stephan-bechert-yFV39g6AZ5o-unsplash.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Kithimani",
+  },
+  {
+    id: "4",
+    Image: "./PropertyImages/todd-kent-178j8tJrNlc-unsplash.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Konza",
+  },
+  {
+    id: "5",
+    Image: "./PropertyImages/vu-anh-TiVPTYCG_3E-unsplash.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Nairobi",
+  },
+  {
+    id: "6",
+    Image: "./PropertyImages/residence-ge8328143b_1920.jpg",
+    description: "lorem",
+    rent: "From $20K",
+    size: "1200 Sqft",
+    bed: "2 Bed",
+    bath: "2 Bathroom",
+    location: "Rongai",
+  },
+];
+export default PropertyDetail;
