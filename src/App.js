0e6f7254be2171742bfebadd01eeb79b67098864
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-12-07 09:15:24 +0300

    home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 import React from "react";
- import Forms from "./components/forms";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+ 
 function App() {
   return (
     <div className="App">
-      <Forms />
-      <Navbar/>
-      <Footer />
+      <Home />
     </div>
   );
 }
